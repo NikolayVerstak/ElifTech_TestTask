98c0f3ff8d7b4cf0db61247ca564942806c4eb9a
Nikolay Verstak <108621811+NikolayVerstak@users.noreply.github.com>
2023-07-15 01:13:28 +0300

    change base url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // define API where a user will make a request to get his data
 export const baseAxios = axios.create({
     // local link
-    baseURL: "http://localhost:3001/api",
+    // baseURL: "http://localhost:3001/api",
 
     // deployed link
-    // baseURL: "https://foodify-delivery-api.onrender.com/api",
+    baseURL: "https://foodify-delivery-api.onrender.com/api",
 });
